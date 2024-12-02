17c5c08c8e9aee2a340959b61c830d7aa42b22f9
Dhruv <dhruvvermaw@gmail.com>
2024-12-02 13:26:24 +0530

    changed frontend url

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://jeinfrontend.vercel.app',
     methods: ['POST','GET','PUT','DELETE'],
     credentials: true
 }));
